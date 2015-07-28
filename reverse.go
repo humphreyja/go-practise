27e769493eee6402c1b356f3e631930ed517dc5c
Jake Humphrey <humhreyja@mnstate.edu>
2015-07-28 12:09:01 -0500

    Adding readline and reverse string

---
+++ reverse.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/shavac/readline"
+
+func main() {
+	prompt := "by your command> "
+	//loop until ReadLine returns nil (signalling EOF)
+L:
+	for {
+		switch result := readline.ReadLine(&prompt); true {
+		case result == nil:
+			println()
+			break L //exit loop with EOF(^D)
+		case *result != "": //ignore blank lines
+			println(*result)
+			readline.AddHistory(*result) //allow user to recall this line
+		}
+	}
+}
